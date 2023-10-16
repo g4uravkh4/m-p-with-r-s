694c5b33b44307464b43cf267cb63a15b66feba0
aura-gv <gauravkhatiwada4@gmail.com>
2023-10-16 17:48:48 +0545

    axios for API call

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import MovieApi from "../../common/api/MovieApi";
+import { APIKey } from "../../common/api/MovieApiKey";
 
 function Home() {
-  return <div>Home</div>;
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(
+        `?APIKey=${APIKey}&s=batman&type=movie`
+      ).catch((err) => {
+        console.log("Err: ", err);
+      });
+      console.log("Response: ", response);
+    };
+
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 }
 
 export default Home;

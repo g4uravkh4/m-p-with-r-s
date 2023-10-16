0c9c1993ab3eed036fad700782e12d7861c5d4f5
aura-gv <gauravkhatiwada4@gmail.com>
2023-10-16 17:34:29 +0545

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Footer.scss";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie</div>
+      <div>Copyright 2023 @movieApp</div>
+    </div>
+  );
 }
 
 export default Footer;

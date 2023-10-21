1a88051b8edc4d5fde19a2faa8d7b36a3ab41f55
aura-gv <gauravkhatiwada4@gmail.com>
2023-10-21 16:10:45 +0545

    dispatch redux actions

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,20 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import MovieApi from "../../common/api/MovieApi";
 import { APIKey } from "../../common/api/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 function Home() {
+  const movieText = "Avengers";
+  const dispatch = useDispatch();
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await MovieApi.get(
-        `?APIKey=${APIKey}&s=batman&type=movie`
+        `?APIKey=${APIKey}&s=${movieText}&type=movie`
       ).catch((err) => {
         console.log("Err: ", err);
       });
-      console.log("Response: ", response);
+      dispatch(addMovies(response.data));
     };
 
     fetchMovies();
